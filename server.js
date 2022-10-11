e611df5f14d5e4a2ff9c75912b9a97fab67019b2
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-11 23:55:30 +0530

    db connection

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 dotenv.config({path: './config.env'})
 
 import app from './app.js'
 
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true
+}).then(con => {
+	console.log(con.connections);
+	console.log(`DB connected sucessfully`);
+})
+
 const port = process.env.PORT || 3000;
 
-console.log(process.env);
+
 
 app.listen(port, () => {
 	console.log(`Listening on ${port}....`);
