c6ddad5a720179b90997e8630483ba48824212bc
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-12 00:08:33 +0530

    add tour mongoose schema

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ mongoose.connect(process.env.DATABASE_URL, {
 	console.log(`DB connected sucessfully`);
 })
 
+
+const tourSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'A tour must have a name'],
+		unique: true
+	},
+	rating: {
+		type: Number,
+		default: 4.5
+	},
+	price: {
+		type: Number,
+		required: [true, 'A tour must have a price']
+	}
+})
+const Tour = mongoose.model('Tour', tourSchema)
+
+
 const port = process.env.PORT || 3000;
 
 
