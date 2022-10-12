bb225fd974da7a622d97d12f6d4dc978e9bd6796
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-12 21:05:36 +0530

    delete tour handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -87,13 +87,20 @@ const updateTour = async (req, res) => {
 	}
 };
 
-const deleteTour = (req, res) => {
-	const id = Number(req.params.id);
-
-	res.status(204).json({
-		status: "success",
-		data: null,
-	});
+const deleteTour = async (req, res) => {
+	try{
+		await Tour.findByIdAndDelete(req.params.id)
+		res.status(204).json({
+			status: "success",
+			data: null,
+		});
+	}
+	catch(err){
+		res.status(400).json({
+			status: "fail",
+			message: err,
+		});
+	}
 };
 
 export { getAllTours, getTour, createTour, updateTour, deleteTour };
