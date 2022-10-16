685bcf94a63fa4db23d0f5ab26958f3edc2c312c
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-16 15:50:46 +0530

    refractored global error handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import tourRouter from './routes/tourRoutes.js'
 import userRouter from './routes/userRoutes.js'
+import AppError from "./utils/appError.js";
+import { errorHandler } from "./controllers/errorController.js";
 
 const app = express();
 
@@ -18,21 +20,16 @@ app.all('*',(req, res, next) => {
   //   message: `Can't find ${req.originalUrl} on this server`
   // })
 
-  const err = new Error(`Can't find ${req.originalUrl} on this server`)
-  err.status = 'fail'
-  err.statusCode = 404
-  next(err);  // if next receives an argument no matter what it is, automatically knows it was an error
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`)
+  // err.status = 'fail'
+  // err.statusCode = 404
+
+  const error = new AppError(404, `Can't find ${req.originalUrl} on this server`)
+  next(error);  // if next receives an argument no matter what it is, automatically knows it was an error
               // will also skip all other middlewares in the stack and go to global error handling middleware
 })
 
-app.use((err, req, res, next) => {      // error handling middleware - 4 params
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || `error`
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(errorHandler)
 
 export default app
 
---
+++ controllers/errorController.js
@@ -0,0 +1,10 @@
+const errorHandler = (err, req, res, next) => {      // error handling middleware - 4 params
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || `error`
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
+}
+
+export { errorHandler }
