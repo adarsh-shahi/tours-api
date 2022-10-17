db06d927575bf08db0783989bfa690d268c21c10
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-17 23:31:18 +0530

    send jwt response in signup route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,21 @@
 import User from "../models/userModel.js";
+import jwt from 'jsonwebtoken'
 
 const signup = async (req, res, next) => {
 	try {
-		const newUser = await User.create(req.body);
+		
+		const newUser = await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password,
+			passwordConfirm: req.body.passwordConfirm
+		});
+
+		const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN})
+
 		res.status(201).json({
 			status: "success",
+			token,
 			user: newUser,
 		});
 	} catch (err) {
